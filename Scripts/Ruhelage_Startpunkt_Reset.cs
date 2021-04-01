1d86274216e8a8977c91a330756e2d6f52e139e0
JoeLebsky <49817266+JoeLebsky@users.noreply.github.com>
2021-04-01 08:03:47 +0200

    Update Ruhelage_Startpunkt_Reset.cs

--- Scripts/Ruhelage_Startpunkt_Reset.cs
+++ Scripts/Ruhelage_Startpunkt_Reset.cs
@@ -1,8 +1,8 @@
 /* versucht alle Bewegungskräfte eines Objekts (welches einen rigidbody hat) auf Tastendruck zurückzusetzen
-Version 2021-03-31
+Version 2021-04-01
 
 Baustellen: 
- - ggf. weitere Angaben erlauben (Rotation, welche Taste)... 
+ - ggf. weitere Angaben erlauben (welche Rotation, welche Taste)... 
  - und manchmal muss man für einen Sekundenbruchteil auf "Objekt ist kinematisch" gehen? (s. Unity-Forumsbeitrag 39998)
  - und Check "Object has rigidbody"
  */
@@ -10,10 +10,13 @@ Baustellen:
 using UnityEngine;
 
 public class Ruhelage_Startpunkt_Reset : MonoBehaviour {
-public Vector3 NeustartPosition = Vector3.zero; // kann der User im Inspector angeben
-private Rigidbody rb;
+public Vector3 NeustartPosition = Vector3.zero; // kann der User im Inspector angeben, alternativ den Startwert;
+[Header("oder:")]
+public bool StattdessenStartposition = true;
+private Rigidbody rb; private Quaternion Startdrehung; private Vector3 Startposition;
 void Start(){
     rb = GetComponent<Rigidbody>();
+    Startdrehung = transform.rotation; Startposition = transform.position;
 }
     
     void Update(){
@@ -24,7 +27,11 @@ void Start(){
             rb.inertiaTensorRotation = Quaternion.identity;
             // das hier auf Null setzen gibt Fehler: rb.inertiaTensor = Vector3.zero;
             // und wieder platzieren wie vom User gewünscht
-            rb.transform.position = NeustartPosition;
+            transform.rotation = Startdrehung;
+            if (StattdessenStartposition==true){
+                transform.position = Startposition;
+            } else
+            { transform.position = NeustartPosition; }
             
         }
     }
